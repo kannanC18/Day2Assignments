25268806aec8c45a9ae13a9cb5ef9bc39f80751e
kannanC18 <kannan.viru005@gmail.com>
2026-03-20 15:57:57 +0530

    Add files via upload

---
+++ Day2Number.js
@@ -0,0 +1,19 @@
+function numbertype(num)
+{
+    if(num>10)
+    {
+        console.log("Poistive")
+    }
+    else if (num<=10)
+        {
+        console.log("negative")
+}
+else
+    {
+    console.log("Neutral")
+}
+}
+
+let num=0
+numbertype(num)
+
